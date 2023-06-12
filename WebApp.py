c56154b0d678f6b538be72d54bf5106e4f3782cd
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-06-12 11:05:14 +0700

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -32,7 +32,7 @@ def main():
     filtered_data = Data[(Data['secondary_category'] == Category) & (Data['skin_tone'] == Skin) & (Data['skin_type'] == Type)]
     if len(filtered_data) > 0:
         st.write("Hasil Pencarian:")
-        st.table(filtered_data[['product_name', 'brand_name', 'review_title', 'loves_count', 'price_usd']])
+        st.table(filtered_data[['product_name', 'brand_name', 'review_title', 'loves_count', 'price_usd', 'review_text]])
     else:
         st.write("Maaf, tidak ada produk yang cocok dengan pilihan Anda.")
             

c02e24d11cfad62fb2f22d714e403bf98914cde2
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-06-12 10:52:36 +0700

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -15,7 +15,15 @@ with st.sidebar:
       
       Type = st.selectbox('Skin Type', Data['skin_type'].unique())
       
-
+# Function to get top 5 similar products
+def get_top_similar_products(product_name):
+        index = indices[product_name]
+        similarity_scores = list(enumerate(cosine_similarities[index]))
+        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+        similarity_scores = similarity_scores[1:6]  # Exclude the same product
+        product_indices = [score[0] for score in similarity_scores]
+        return product_names.iloc[product_indices]
+      
 # Dashboard
 def main():
     st.title("Beauty Things")
@@ -28,20 +36,8 @@ def main():
     else:
         st.write("Maaf, tidak ada produk yang cocok dengan pilihan Anda.")
             
-    # Jika ada produk yang cocok dengan pilihan pengguna
-    if not filtered_data.empty:
-    # Define TF-IDF vectorizer
-    vectorizer = TfidfVectorizer()
-
-    # Apply TF-IDF vectorizer to review text
-    tfidf_matrix = vectorizer.fit_transform(filtered_data['review_text'])
-
-    # Calculate cosine similarity matrix
-    cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-    # Get product names and indices
-    product_names = filtered_data['product_name']
-    indices = pd.Series(filtered_data.index, index=filtered_data['product_name'])
+    product_name = get_top_similar_products(product_name)
+            
 
 if __name__ == "__main__":
     main()

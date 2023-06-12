849423813cef11f8ca1f22b59156fa5c372976c4
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-06-12 13:03:44 +0700

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -15,15 +15,6 @@ with st.sidebar:
       
       Type = st.selectbox('Skin Type', Data['skin_type'].unique())
       
-# Function to get top 5 similar products
-def get_top_similar_products(product_name):
-        index = indices[product_name]
-        similarity_scores = list(enumerate(cosine_similarities[index]))
-        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-        similarity_scores = similarity_scores[1:6]  # Exclude the same product
-        product_indices = [score[0] for score in similarity_scores]
-        return product_names.iloc[product_indices]
-      
 # Dashboard
 def main():
     st.title("Beauty Things")
@@ -35,8 +26,6 @@ def main():
         st.table(filtered_data[['product_name', 'brand_name', 'review_title', 'loves_count', 'price_usd', 'review_text']])
     else:
         st.write("Maaf, tidak ada produk yang cocok dengan pilihan Anda.")
-            
-    product_name = get_top_similar_products(product_name)
             
 
 if __name__ == "__main__":

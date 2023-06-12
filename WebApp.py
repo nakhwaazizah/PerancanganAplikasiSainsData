324f5a121a23cd23c415ddb5b47ec4078ad4f2c8
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-06-12 13:45:47 +0700

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -1,16 +1,9 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as pt
-import seaborn as sns
-import numpy as np
+import streamlit as st 
+import pandas as pd 
+import numpy as np 
 import plotly.express as px
-import pandas_bokeh
-import requests
-import json
-import bokeh.plotting as bpl
-from bokeh.palettes import Spectral11
-from bokeh.models import NumeralTickFormatter, Legend
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 url = 'https://raw.githubusercontent.com/nakhwaazizah/Sephora/main/Results%20dataset%20Sephora.csv'
 Data = pd.read_csv(url)
 

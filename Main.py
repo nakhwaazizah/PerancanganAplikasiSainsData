42d90b55b5ebc33ce0eb8a58368843e1c0708984
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-06-12 13:59:05 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,3 @@
-def main():
-    # Halaman Utama (Home)
-    st.title("Beauty Things")
-    st.write("Let's find skincare for you!")
-
-
-if __name__ == "__main__":
-    main()
+st.title("Beauty Things")
+st.write("Let's find skincare for you!")
+    

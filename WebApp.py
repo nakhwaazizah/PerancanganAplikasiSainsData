e287e04cc8195d26281d8f18fa37769ce19c3e0e
nakhwaazizah <93728502+nakhwaazizah@users.noreply.github.com>
2023-06-12 15:47:51 +0700

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -56,7 +56,20 @@ def main():
         st.table(filtered_data[['product_name', 'brand_name', 'review_title', 'loves_count', 'price_usd', 'review_text']])
     else:
         st.write("Maaf, tidak ada produk yang cocok dengan pilihan Anda.")
-    
+    # Jika ada produk yang cocok dengan pilihan pengguna
+    if not filtered_data.empty:
+    # Define TF-IDF vectorizer
+        vectorizer = TfidfVectorizer()
+
+    # Apply TF-IDF vectorizer to review text
+        tfidf_matrix = vectorizer.fit_transform(filtered_data['review_text'])
+
+    # Calculate cosine similarity matrix
+        cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    # Get product names and indices
+        product_names = filtered_data['product_name']
+        indices = pd.Series(filtered_data.index, index=filtered_data['product_name'])
             
 
 if __name__ == "__main__":
